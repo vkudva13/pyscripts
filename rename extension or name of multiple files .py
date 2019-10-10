76b969c9a7a21147f0653d0db8d0c29959259c19
vkudva13 <49591507+vkudva13@users.noreply.github.com>
2019-10-10 20:04:20 +0530

    Add files via upload

---
+++ rename extension or name of multiple files .py
@@ -0,0 +1,15 @@
+import os
+
+srcpath = "E:/tv shows temp/example"
+dstpath = srcpath #change this to move files to this path
+extensionBefore = ".mp4"
+extensionAfter = ".mp3" 
+
+for filename in os.listdir(srcpath):
+    name=filename[ :-len(extensionBefore)]
+    #name can be modified here
+	
+    if filename[ len(name):] == extensionBefore:#renames all files with extensionBefore
+        dst = dstpath + "/"+str(name) + extensionAfter
+        src =srcpath + "/"+str(name) + extensionBefore
+        os.rename(src, dst)
